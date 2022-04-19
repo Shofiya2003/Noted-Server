7d6763dae4d327f6cb2e497a62d5bd74e1b153a1
Shofiya2003 <shofiyabootwala@gmail.com>
2022-04-19 13:04:12 +0530

    added the validator file

---
+++ backend/utils/validators/auth.js
@@ -0,0 +1,22 @@
+const { body } = require("express-validator");
+const validators = {
+  post: {
+    "/signup": [
+      body("username").exists().withMessage("username is missing"),
+      body("email")
+        .exists()
+        .withMessage("email is missing")
+        .isEmail()
+        .withMessage("email is invalid"),
+      body("password").exists().withMessage("password is missing"),
+    ],
+    "/signin": [
+      body("email")
+        .exists()
+        .withMessage("email is missing")
+        .isEmail()
+        .withMessage("email is invalid"),
+      body("password").exists().withMessage("password is missing"),
+    ],
+  },
+};
