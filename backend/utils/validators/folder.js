63079dbf9a83bf0f1b15bb631cc3d1c0aebc72bf
Shofiya2003 <shofiyabootwala@gmail.com>
2022-04-19 13:46:12 +0530

    validator for video api

--- backend/utils/validators/folder.js
+++ backend/utils/validators/folder.js
@@ -1,25 +1,31 @@
+const { body, query } = require("express-validator");
+const validators = {
+  post: {
+    "/create": [
+      body("folder_name").exists().withMessage("folder name is missing"),
+    ],
+    "/delete": [
+      body("folder_name").exists().withMessage("folder name is missing"),
+    ],
+    "/editname": [
+      body("new_folder_name")
+        .exists()
+        .withMessage("new folder name is missing"),
+      body("old_folder_name")
+        .exists()
+        .withMessage("old folder name is missing"),
+    ],
+  },
 
-const {body,query}=require('express-validator');
-const validators={
-    "/post":{
-        "/create":[
-            body("folder_name").exists().withMessage("folder name is missing")
-        ],
-        "/delete":[
-            body("folder_name").exists().withMessage("folder name is missing")
-        ],
-        "/editname":[
-            body("new_folder_name").exists().withMessage("new folder name is missing"),
-            body("old_folder_name").exists().withMessage("old folder name is missing"),
-        ]
-    },
+  get: {
+    "/getfolders": [
+      query("deleted")
+        .exists()
+        .withMessage("deleted query missing")
+        .isBoolean({ loose: true })
+        .withMessage("query must be 'true' of 'false' "),
+    ],
+  },
+};
 
-    "/get":{
-        "/getfolders":[
-            query("deleted").isBoolean({loose:true}).withMessage("query must be 'true' of 'false' ")
-        ]
-    }
-}
-
-
-module.exports=validators;
+module.exports = validators;
---
+++ backend/utils/validators/video.js
@@ -0,0 +1,25 @@
+const {body,param}=require('express-validator');
+
+const validators={
+    post:{
+        '/delete':[
+            body('video_id').exists().withMessage('video_id is missing')
+        ],
+        '/editname':[
+            body('video_id').exists().withMessage('video_id is missing'),
+            body('new_video_name').exists().withMessage('new_video_name is missing')
+            
+        ],
+        '/changefolder':[
+            body('video_id').exists().withMessage('video_id is missing'),
+            body('folder_name').exists().withMessage('folder_name is missing')
+
+        ]
+
+    },
+    get:{
+        '/':[
+            param('video_id').exists().withMessage('param video_id is missing')
+        ]
+    }
+}
