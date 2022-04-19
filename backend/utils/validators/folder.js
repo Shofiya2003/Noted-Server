f7e36892aabd3555cae4c89b401f7ed952a680ac
Shofiya2003 <shofiyabootwala@gmail.com>
2022-04-19 13:30:20 +0530

    added a query validator for get all apis

--- backend/utils/validators/folder.js
+++ backend/utils/validators/folder.js
@@ -3,15 +3,23 @@ const {body,query}=require('express-validator');
 const validators={
     "/post":{
         "/create":[
-            body('folder_name').exists().withMessage('folder name is missing')
+            body("folder_name").exists().withMessage("folder name is missing")
         ],
         "/delete":[
-            body('folder_name').exists().withMessage('folder name is missing'),
-            query('deleted').isBoolean({loose:true}).withMessage('query must be true or false')
+            body("folder_name").exists().withMessage("folder name is missing")
         ],
         "/editname":[
-            body('new_folder_name').exists().withMessage('folder name is missing'),
-            body('old_folder_name').exists().withMessage('folder name is missing'),
+            body("new_folder_name").exists().withMessage("new folder name is missing"),
+            body("old_folder_name").exists().withMessage("old folder name is missing"),
+        ]
+    },
+
+    "/get":{
+        "/getfolders":[
+            query("deleted").isBoolean({loose:true}).withMessage("query must be 'true' of 'false' ")
         ]
     }
 }
+
+
+module.exports=validators;
