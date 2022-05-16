dd2fa526d3ca0c00b8283e61488bc4ba6891328b
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-16 11:30:30 +0530

    commented getall videos api. bug to be resolved

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const requestHandler=require('./requestHandler');
 const video =require('../managers/video');
-router.get("/",requestHandler.handlerequest(async (req,res)=>{
-    const { user_id } = req.decodedTokenData;
-    const videos=await video.getAll({user_id,query:req.query});
-     return res.status(200).json({message:'success',videos})
-  }))
+
+//Get all videos bug to be solved
+// router.get("/",requestHandler.handlerequest(async (req,res)=>{
+//     const { user_id } = req.decodedTokenData;
+//     const videos=await video.getAll({user_id,query:req.query});
+//      return res.status(200).json({message:'success',videos})
+//   }))
 
 module.exports = router;
--- backend/services/video.js
+++ backend/services/video.js
@@ -80,21 +80,31 @@ funcs.changeFolder=async ({video_id, user_id, folder_name})=>{
 
 }
 
-funcs.getAll=async ({user_id,query})=>{
-    const page=parseInt(query.page);
-    const limit=parseInt(query.limit);
-    const deleted=query.deleted;
-    const startIndex = (page - 1) * limit
-    const endIndex = page * limit
-    const totalDocuments= await Video.countDocuments({user_id });
 
-    const result = await Video.find({user_id,is_deleted:deleted==='true' },{ video_id: 1, video_name: 1,video_url:1 }).skip(startIndex).limit(limit);
-    if (!result) {
-      res.status(404).json({ message: "no video not found" });
-    } else {
-      res.status(200).json({ message: "success",result });
-    }
-  } 
+//Get all videos bug to be solved
+
+// funcs.getAll=async ({user_id,query})=>{
+//     try{
+//       const page=parseInt(query.page);
+//       const limit=parseInt(query.limit);
+//       const deleted=query.deleted;
+//       const startIndex = (page - 1) * limit
+//       const endIndex = page * limit
+//       const totalDocuments= await Video.countDocuments({user_id });
+  
+//       const result = await Video.find({user_id,is_deleted:deleted==='true' },{ video_id: 1, video_name: 1,video_url:1 }).skip(startIndex).limit(limit);
+//       if (!result) {
+//         res.status(404).json({ message: "no video not found" });
+//       } else {
+//         res.status(200).json({ message: "success",result });
+//       }
+//     }catch(err){
+//       throw {
+        
+//       }
+//     }
+    
+//   } 
 
 
 module.exports = funcs;
